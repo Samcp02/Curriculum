90d5fbc16ffaed51d0ad6fabf219c19c7dc80d5f
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-11 13:34:30 +0100

    borrador javascript

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-function main() {
-    console.log("starting...");
+function imprimeHola() {
+    console.log('Hola');
 }
 
-window.addEventListener("load", main)
+function main() {
+    console.log("starting...");
+    const titulo = document.querySelector('h1');
+    titulo.addEventListener('pointerenter', imprimeHola)
+
+    const elements = Array.from (document.body.querySelectorAll('*') 
+    );
+    console.log(elements);
+}
+
+const counter = elements.map(
+    (item)=>{
+        return{
+            element: item,
+            counter: 0,
+        }
+    }
+);
+    console.log(counter);
+    
+
+// con esto le decimos que cuando termine de ejecutar la página ejecutar la función (main)
+window.addEventListener('load', main)
+
